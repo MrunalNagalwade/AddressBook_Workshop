b0863aca028ae63a7719ea860b1384f976fdeeff
MrunalNagalwade <79105990+MrunalNagalwade@users.noreply.github.com>
2021-05-15 10:58:48 -0700

    Update AddressBookTest.java

--- src/test/java/addressbook/AddressBookTest.java
+++ src/test/java/addressbook/AddressBookTest.java
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class AddressBookTest {
@@ -102,6 +106,25 @@ public class AddressBookTest {
         System.out.println(addressBookStateList.toString());
         Assertions.assertEquals(1, addressBookCityList.size());
         Assertions.assertEquals(2, addressBookStateList.size());
+    }
+    @Test
+    public void given4Entries_WhenAdded_ShouldGetAdded()
+    {
+        Contact[] arrayOfContactOne = new Contact[]{
+                new Contact("Sayi", "Verma", "Shiv nagar",
+                        "Gondia", "MH", "s@gmail.com", 8909878987L, 611000, LocalDate.now()),
+                new Contact("Sonali", "Wani", "Amaravati",
+                        "Amaravati ", "Mh", "a@gmail.com", 9087897867L, 611001, LocalDate.now()),
+
+        };
+        AddressBookService addressBookService = new AddressBookService();
+        addressBookService.readPersonDetailsDataDB(AddressBookService.IOService.DB_IO);
+        Instant start = Instant.now();
+        addressBookService.multipleDataAdded(Arrays.asList(arrayOfContactOne));
+        Instant end = Instant.now();
+        System.out.println("Time Took: " + Duration.between(start, end));
+        boolean status = addressBookService.checkDataInDB("Sonali");
+        Assertions.assertTrue(status);
     }
 
 }
